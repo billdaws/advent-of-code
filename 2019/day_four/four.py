30dc261b855988ab465144d045b641d4b171bbb8
wdaws <bill.d.daws@gmail.com>
2019-12-05 21:42:03 -0500

    working day four part one

--- 2019/day_four/four.py
+++ 2019/day_four/four.py
@@ -2,8 +2,15 @@ import os
 this_path = os.path.dirname(__file__)
 inpath = os.sep.join([this_path, "four_input.txt"])
 
+
 def is_six_digits(x: int) -> bool:
+    """This function isn't actually necessary given how I'll be generating the
+    input.
+
+    Oh well.
+    """
     return len(str(x)) == 6
+
 
 def within_range(x: int, lo: int, hi: int) -> bool:
     """This function isn't actually necessary given how I'll be generating the
@@ -13,13 +20,30 @@ def within_range(x: int, lo: int, hi: int) -> bool:
     """
     return lo <= x <= hi
 
+
 def has_adjacent_double(x: int) -> bool:
     x_str = str(x)
     for i in range(len(x_str) - 1):
         if x_str[i] == x_str[i + 1]:
             return True
-    
+
     return False
+
+
+def has_adjacent_double_v2(x: int) -> bool:
+    """Like has_adjacent_double, but this time makes sure that the adjacent
+    double is not actually part of a longer run of the same digit.
+    
+    Note that this approach only works on values that passed part 1's 
+    constraints.
+    """
+    x_str = str(x)
+    for n in x_str:
+        if x_str.count(n) == 2:
+            return True
+        
+    return False
+
 
 """
 Note that 'has_adjacent_double' and 'digits_never_decrease' could be
@@ -29,18 +53,19 @@ that the range of inputs and the size of each input is small.
 If performance were a concern, this would be an obvious target for optimization.
 """
 
+
 def digits_never_decrease(x: int) -> bool:
     while x > 0:
         lsd = x % 10
-        x = int(x / 10) # int conversion just to deal with mypy warning
-                        # this operation shouldn't cause any problems
-                        # because an int divided by 10 should not encounter
-                        # round-off issues
+        x = int(x / 10)  # int conversion just to deal with mypy warning
+        # this operation shouldn't cause any problems
+        # because an int divided by 10 should not encounter
+        # round-off issues
         if lsd < x % 10:
             return False
-    
+
     return True
-    
+
 
 if __name__ == "__main__":
     with open(inpath, "r") as f:
@@ -48,11 +73,14 @@ if __name__ == "__main__":
         lo = int(str_lo)
         hi = int(str_hi)
 
-    accepted = 0
+    part_one_accepted = 0
+    part_two_accepted = 0
     for x in range(lo, hi + 1):
-        if is_six_digits(x) and \
-            has_adjacent_double(x) and \
-            digits_never_decrease(x):
-            accepted += 1
-
-    print(accepted)
+        if digits_never_decrease(x):
+            if has_adjacent_double(x):
+                part_one_accepted += 1
+                if has_adjacent_double_v2(x):
+                    part_two_accepted += 1
+        
+    print(f"Part one has {part_one_accepted} acceptable answers.")
+    print(f"Part two has {part_two_accepted} acceptable answers.")
